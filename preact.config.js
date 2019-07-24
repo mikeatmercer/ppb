640fb179b48412b08ca2b1482bd7f8ef3f028c94
mike moore <mike.moore1@mercer.com>
2019-07-24 09:12:10 -0400

    working, just a really big bundle size

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
   config.output.filename = "[name].js";
@@ -5,12 +7,24 @@ export default (config, env, helpers) => {
 
   let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
   plugin.options.disable = true;
-
+  config.plugins.push(new BundleAnalyzerPlugin());
   if (env.production) {
+    console.log(config);
     config.output.libraryTarget = "umd";
     let {rule} = helpers.getLoadersByName(config, "css-loader")[0];
+    //console.log(rule);
+    rule.loader[1] = {
+      loader: "style-loader",
+      options: {
+        outputStyle: "compressed"
+      }
+    }
     rule.loader[2].options.localIdentName = '[sha1:hash:hex:5]';
+    console.log(rule.loader[3].options)
     let { plugin } = helpers.getPluginsByName(config, "UglifyJsPlugin")[0];
-  //  plugin.options.sourceMap = false
+   // plugin.options.sourceMap = false
+   //config.plugins.push(new BundleAnalyzerPlugin({analyzerMode:"static", generateStatsFile: true}));
+   
   }
+  
 };
